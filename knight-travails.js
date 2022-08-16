f78992d5bfe0223a6d6823d3f6c735d0afa22fb9
GBN6 <czesiox8@gmail.com>
2022-08-16 15:00:21 +0200

    Create knight travails function

---
+++ knight-travails.js
@@ -0,0 +1,14 @@
+const squareRegistry = new Map();
+
+const chessTile = (x, y) => {
+    const xPosition = x
+    const yposition = y;
+    let predecessor;
+
+    const knightOffests = [
+        [1,2], [1,-2],
+        [2,1], [2,-1],
+        [-1,2], [-1,-2],
+        [-2,1], [-2,-1]
+    ]
+}
